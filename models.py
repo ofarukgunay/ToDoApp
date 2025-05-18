6b43df4ccde433547e678d7c6af716ae01c9acf6
Ömer Faruk Günay <116501544+ofarukgunay@users.noreply.github.com>
2025-05-18 19:51:37 +0300

    Auth endpoint added

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from database import Base
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+
 
 class Todo(Base):
     __tablename__ = 'todos'
@@ -9,6 +10,18 @@ class Todo(Base):
     description = Column(String)
     priority = Column(Integer)
     complete = Column(Boolean, default=False)
+    owner_id = Column(Integer, ForeignKey('users.id'))
 
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True)
+    username = Column(String, unique=True)
+    firstname = Column(String)
+    lastname = Column(String)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+    role = Column(String)
 
 
---
+++ routers/todo.py
@@ -0,0 +1,69 @@
+from pydantic import BaseModel, Field
+from typing import Annotated
+from fastapi import Depends, Path, HTTPException, APIRouter
+from sqlalchemy.orm import Session
+from starlette import status
+from models import Base, Todo
+from database import engine, SessionLocal
+
+router = APIRouter(
+    prefix="/todo",
+    tags=["Todo"],
+)
+
+
+class TodoRequest(BaseModel):
+    title: str = Field(min_length=3)
+    description: str = Field(min_length=3, max_length=1000)
+    priority: int = Field(gt=0, lt=6)
+    complete: bool
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+@router.get("/get_all")
+async def read_all(db: db_dependency):
+    return db.query(Todo).all()
+
+@router.get("/get_by_id/{todo_id}", status_code=status.HTTP_200_OK)
+async def read_by_id(db: db_dependency, todo_id: int = Path(gt=0)):
+    todo = db.query(Todo).filter(Todo.id == todo_id).first()
+    if todo is not None:
+        return todo
+    raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail="Todo not found")
+
+@router.post("/create", status_code=status.HTTP_201_CREATED)
+async def create_todo(db: db_dependency, todo_request: TodoRequest):
+    todo = Todo(**todo_request.dict())
+    db.add(todo)
+    db.commit()
+
+@router.put("/update/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def update_todo(db: db_dependency, todo_request: TodoRequest, todo_id: int = Path(gt=0)):
+    todo = db.query(Todo).filter(Todo.id == todo_id).first()
+    if todo is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Todo not found")
+    todo.title = todo_request.title
+    todo.description = todo_request.description
+    todo.priority = todo_request.priority
+    todo.complete = todo_request.complete
+
+    db.add(todo)
+    db.commit()
+
+@router.delete("/delete/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_todo(db: db_dependency, todo_id: int = Path(gt=0)):
+    todo = db.query(Todo).filter(Todo.id == todo_id).first()
+    if todo is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Todo not found")
+    #db.query(Todo).filter(Todo.id == todo_id).delete() Bu daha garanti silme yöntemi!!!
+    db.delete(Todo)
+    db.commit()
+
