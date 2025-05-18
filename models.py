558abd5de4ba55b24ce604087719de3da1d24a52
Ömer Faruk Günay <116501544+ofarukgunay@users.noreply.github.com>
2025-05-18 17:13:50 +0300

    Endpoints Added

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 from database import Base
 from sqlalchemy import Column, Integer, String, Boolean
 
-class ToDo(Base):
+class Todo(Base):
     __tablename__ = 'todos'
 
     id = Column(Integer, primary_key=True, index=True)
