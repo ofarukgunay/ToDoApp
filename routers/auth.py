59215efdd146b6a389c8eb4aa2867a183e7c35a8
Ömer Faruk Günay <116501544+ofarukgunay@users.noreply.github.com>
2025-05-19 17:19:00 +0300

    Update auth

--- routers/auth.py
+++ routers/auth.py
@@ -1,7 +1,45 @@
-from fastapi import APIRouter
-
+from fastapi import APIRouter, Depends
+from pydantic import BaseModel
+from models import User
+from passlib.context import CryptContext
+from database import SessionLocal
+from typing import Annotated
+from sqlalchemy.orm import Session
 
 router = APIRouter(
     prefix="/auth",
     tags=["Authentication"],
 )
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+class CreateUserRequest(BaseModel):
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    password: str
+    role: str
+
+@router.post("/auth")
+async def create_user(db: db_dependency, create_user_request: CreateUserRequest):
+    user = User(
+        username=create_user_request.username,
+        email=create_user_request.email,
+        firstname=create_user_request.first_name,
+        lastname=create_user_request.last_name,
+        role=create_user_request.role,
+        is_active=True,
+        hashed_password=bcrypt_context.hash(create_user_request.password),
+    )
+    db.add(user)
+    db.commit()
